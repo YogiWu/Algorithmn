8b4fda762911006ef932b8f787b42fda85edb02c
yogi <yogiwuproxy@gmail.com>
2019-10-14 11:45:59 +0800

    finish the recursive solution of the optimal BST

---
+++ optimal_BST/main.cpp
@@ -0,0 +1,38 @@
+//
+// Created by yogi on 2019/9/27.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include "array_utils.h"
+
+#include "recursive.cpp"
+
+using namespace std;
+
+int main ()
+{
+    const int n = 10;
+
+    vector<int> keyList = ArrayUtils::genrateRandomVector(n, 0, 100);
+    sort(keyList.begin(), keyList.end());
+
+    ArrayUtils::printVector(keyList);
+
+    vector<int> proList = ArrayUtils::genrateRandomVector(2*n+1, 0, 100);
+    double total = accumulate(proList.begin(), proList.end(), 0);
+
+    vector<double> keyProList;
+    for (auto &x:proList) {
+        keyProList.push_back(x/total);
+    }
+
+    ArrayUtils::printVector(keyProList);
+    cout << accumulate(keyProList.begin(), keyProList.end(), 0.0) << endl;
+
+    cout << recursive::optimal_BST(keyList, keyProList, 0, keyList.size()-1) << endl;
+
+    return 0;
+}
---
+++ optimal_BST/recursive.cpp
@@ -0,0 +1,36 @@
+//
+// Created by yogi on 2019/10/14.
+//
+#include <iostream>
+#include <cassert>
+#include <limits>
+
+namespace recursive{
+    double optimal_BST (const vector<int> &keyList, const vector<double> &keyProList, int l, int r) {
+        assert(l >=0 && l <= r && r < keyList.size());
+        if (l == r) {
+            return keyProList[2*l+1]*2+keyProList[2*l]+keyProList[2*(l+1)];
+        }
+
+        if (l == r -1) {
+            double left_res = optimal_BST(keyList, keyProList, l, l);
+            double right_res = optimal_BST(keyList, keyProList, r, r);
+
+            double left_root_res = left_res + right_res + keyProList[2*l] + keyProList[2*l+1] - keyProList[2*(l+1)];
+            double right_root_res = left_res + right_res + keyProList[2*(r+1)] + keyProList[2*l+1] - keyProList[2*r];
+
+            return min(left_root_res, right_root_res);
+        }
+
+        double min = numeric_limits<double>::max();
+        for(int i=l+1; i<r; i++) {
+            double left_res = optimal_BST(keyList, keyProList, l, i-1) + accumulate(keyProList.begin()+l*2, keyProList.begin()+i*2+1, 0.0);
+            double right_res = optimal_BST(keyList, keyProList, i+1, r) + accumulate(keyProList.begin()+(i+1)*2, keyProList.begin()+r*2+3, 0.0);
+            double res = left_res + right_res + keyProList[2*i +1];
+
+            min = res < min ? res : min;
+        }
+
+        return min;
+    }
+}
